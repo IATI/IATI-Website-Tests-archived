5b7d0f4c60fb42d9564793667227ff373cb65258
dalepotter <dalepotter@gmail.com>
2016-11-30 17:11:11 +0000

    Streamline test directory to isolate issue

--- tests/test_iati_support.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from web_test_base import *
-
-class TestIATISupport(WebTestBase):
-    requests_to_load = {
-        'IATI Support Landing Page': {
-            'url': 'http://support.iatistandard.org/hc/en-us'
-        }
-    }
